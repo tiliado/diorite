0f08d18a813237046c59cc33c4d693c24ca2f125
Jiří Janoušek <janousek.jiri@gmail.com>
2014-02-03 22:46:40 +0100

    Tweaked env set-up.

--- setup_env_linux.sh
+++ setup_env_linux.sh
@@ -1,3 +1,11 @@
-LD_LIBRARY_PATH=./build
-PATH="./build:$PATH"
+LD_LIBRARY_PATH=$PWD/build
+PATH="$PWD/build:$PATH"
 alias configure="./waf configure"
+if [ -z "$PKG_CONFIG_PATH" ]; then
+	PKG_CONFIG_PATH="$PWD/build:/usr/lib/pkgconfig"
+else
+	PKG_CONFIG_PATH="$PWD/build:$PKG_CONFIG_PATH"
+fi
+CFLAGS="$CFLAGS -I$PWD/build -L$PWD/build"
+
+export CFLAGS PKG_CONFIG_PATH PATH LD_LIBRARY_PATH
