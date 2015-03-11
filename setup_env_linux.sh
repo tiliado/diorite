1a3c628e1e315f996f89e72b73f188f220a0dbf3
Jiří Janoušek <janousek.jiri@gmail.com>
2015-03-11 08:27:24 +0100

    Add more aliases to setup_env_linux
    configure, rebuild, fedora_configure, fedora_rebuild, update
    
    Signed-off-by: Jiří Janoušek <janousek.jiri@gmail.com>

--- setup_env_linux.sh
+++ setup_env_linux.sh
@@ -1,6 +1,5 @@
 LD_LIBRARY_PATH=$PWD/build
 PATH="$PWD/build:$PATH"
-alias configure="./waf configure"
 if [ -z "$PKG_CONFIG_PATH" ]; then
 	PKG_CONFIG_PATH="$PWD/build:/usr/lib/pkgconfig"
 else
@@ -9,3 +8,9 @@ fi
 CFLAGS="$CFLAGS -I$PWD/build -L$PWD/build"
 CC="gcc"
 export CC CFLAGS PKG_CONFIG_PATH PATH LD_LIBRARY_PATH
+
+alias configure="./waf configure"
+alias rebuild="./waf distclean configure build"
+alias fedora_configure="./waf configure --libdir /usr/local/lib64"
+alias fedora_rebuild="./waf distclean configure build --libdir /usr/local/lib64"
+alias update="./waf && sudo ./waf install"
