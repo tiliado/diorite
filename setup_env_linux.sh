293914e7a969f286853299b609919045e5ef6364
Jiří Janoušek <janousek.jiri@gmail.com>
2017-01-15 18:08:14 +0100

    Use python3 waf in setup_env_linux.sh
    Illegal instruction error occurs inside NuvolaCoreDeveloperKit
    otherwise.
    
    Signed-off-by: Jiří Janoušek <janousek.jiri@gmail.com>

--- setup_env_linux.sh
+++ setup_env_linux.sh
@@ -9,8 +9,8 @@ CFLAGS="$CFLAGS -I$PWD/build -L$PWD/build"
 CC="gcc"
 export CC CFLAGS PKG_CONFIG_PATH PATH LD_LIBRARY_PATH
 
-alias configure="./waf configure --with-experimental-api"
-alias rebuild="./waf distclean configure build --with-experimental-api"
-alias fedora_configure="./waf configure --libdir /usr/local/lib64"
-alias fedora_rebuild="./waf distclean configure build --libdir /usr/local/lib64"
-alias update="./waf && sudo ./waf install"
+alias configure="python3 ./waf configure --with-experimental-api"
+alias rebuild="python3 ./waf distclean configure build --with-experimental-api"
+alias fedora_configure="python3 ./waf configure --libdir /usr/local/lib64"
+alias fedora_rebuild="python3 ./waf distclean configure build --libdir /usr/local/lib64"
+alias update="python3 ./waf && sudo python3 ./waf install"
