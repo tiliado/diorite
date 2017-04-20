b9aafbfb662e6f9607e6ec0376675496b945a4b9
Jiří Janoušek <janousek.jiri@gmail.com>
2017-04-21 00:46:28 +0200

    Remove setup_env_win32
    Signed-off-by: Jiří Janoušek <janousek.jiri@gmail.com>

--- setup_env_win32.sh
+++
@@ -1,5 +0,0 @@
-export WIN32=/usr/i686-w64-mingw32/sys-root/mingw
-export PKG_CONFIG_PATH=$WIN32/lib/pkgconfig
-export CC=i686-w64-mingw32-gcc
-export DISPLAY=:0
-alias configure="./waf configure --platform=win --prefix=$WIN32"
