72e891937f13207509efc1c423d42d2855cee2cf
Jiří Janoušek <janousek.jiri@gmail.com>
2018-12-23 10:58:56 +0100

    Fix whitespace in setup*.sh scripts
    Signed-off-by: Jiří Janoušek <janousek.jiri@gmail.com>

--- setup_env.sh
+++ setup_env.sh
@@ -1,9 +1,9 @@
 LD_LIBRARY_PATH=$PWD/build
 PATH="$PWD/build:$PATH"
 if [ -z "$PKG_CONFIG_PATH" ]; then
-	PKG_CONFIG_PATH="$PWD/build:/usr/lib/pkgconfig"
+    PKG_CONFIG_PATH="$PWD/build:/usr/lib/pkgconfig"
 else
-	PKG_CONFIG_PATH="$PWD/build:$PKG_CONFIG_PATH"
+    PKG_CONFIG_PATH="$PWD/build:$PKG_CONFIG_PATH"
 fi
 CFLAGS="$CFLAGS -I$PWD/build -L$PWD/build"
 CC="gcc"
@@ -15,6 +15,6 @@ alias update="python3 ./waf && sudo python3 ./waf install"
 
 rebuild()
 {
-	python3 ./waf distclean configure build "$@" \
-	&& build/run-dioritetests
+    python3 ./waf distclean configure build "$@" \
+    && build/run-dioritetests
 }
