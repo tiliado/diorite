e06b822da92a8d55e154732a1e8389a668a414ce
Jiří Janoušek <janousek.jiri@gmail.com>
2018-12-23 20:08:37 +0100

    setup_nuvolacdk: add valgrind-tests() func
    It runs tests with Valgrind memory check.
    
    Signed-off-by: Jiří Janoušek <janousek.jiri@gmail.com>

--- setup_nuvolacdk.sh
+++ setup_nuvolacdk.sh
@@ -23,3 +23,14 @@ rebuild()
     python3 ./waf distclean configure build --flatpak "$@" \
     && build/run-dioritetests
 }
+
+valgrind-tests() {
+    python3 ./waf \
+    && G_DEBUG=gc-friendly G_SLICE=always-malloc valgrind \
+        --leak-check=full --leak-resolution=high --num-callers=20 \
+        --show-leak-kinds=definite,indirect,possible \
+        --suppressions=/usr/share/glib-2.0/valgrind/glib.supp \
+        --suppressions=src/diorite.supp \
+        --log-file=valgrind-tests.log ./build/run-dioritetests "$@" \
+    && less valgrind-tests.log
+}
